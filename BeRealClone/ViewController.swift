053cda99a66decf8b84cc8ec7ac1d95be4b1655b
Kobe Petrus <kobe.petrus07@gmail.com>
2023-03-16 00:03:49 -0400

    Initial Commit

---
+++ BeRealClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeRealClone
+//
+//  Created by Kobe Petrus on 3/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
